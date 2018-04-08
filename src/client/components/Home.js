5e1201333d0bcb655450ad658623c22143d9c94f
evilosa <bojkov@sorc.ru>
2018-04-08 20:50:27 +0200

    First commit

---
+++ src/client/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <div>Home VERY BEST component</div>
+      <button onClick={() => console.log('Pressed')}>Press me!</button>
+    </div>
+  );
+};
+
+export default Home;
